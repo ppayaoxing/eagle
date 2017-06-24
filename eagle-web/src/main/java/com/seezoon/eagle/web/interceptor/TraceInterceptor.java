c24f9525f2005af20eb5cab55295c54bfaca743a
734839030 <734839030@qq.com>
2017-06-24 14:27:10 +0800

    add eagle-web

---
+++ eagle-web/src/main/java/com/seezoon/eagle/web/interceptor/TraceInterceptor.java
@@ -0,0 +1,43 @@
+package com.seezoon.eagle.web.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.util.StopWatch;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.seezoon.eagle.core.service.BaseService;
+import com.seezoon.eagle.core.utils.NDCUtils;
+
+public class TraceInterceptor extends BaseService implements HandlerInterceptor {
+	
+	ThreadLocal<StopWatch> threadLocal = new ThreadLocal<>();
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		NDCUtils.push();
+		StopWatch watch = new StopWatch();
+		watch.start();
+		threadLocal.set(watch);
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		StopWatch stopWatch = threadLocal.get();
+		stopWatch.stop();
+		String requestURI = request.getRequestURI();
+		logger.info("{} comleted use {} ms",requestURI,stopWatch.getTotalTimeMillis());
+		threadLocal.remove();
+		NDCUtils.clear();
+	}
+
+	
+}
